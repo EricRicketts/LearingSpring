f1e1116e658e0e379aaf117cb36f5d5a665c5168
Eric Ricketts <eric_ricketts@me.com>
2022-03-01 13:22:45 -0500

    An empty Spring Boot Project just to understand the structure of a Spring Boot project

---
+++ BeginningSpringBoot/empty_project/src/test/java/com/example/empty_project/EmptyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empty_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
