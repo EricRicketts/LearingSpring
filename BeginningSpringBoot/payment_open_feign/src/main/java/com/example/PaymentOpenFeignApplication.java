036877fdab9e4c4412d6d6307167c0b7745fbbcd
Eric Ricketts <eric_ricketts@me.com>
2022-04-04 16:05:22 -0400

    consuming an api

---
+++ BeginningSpringBoot/payment_open_feign/src/main/java/com/example/PaymentOpenFeignApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentOpenFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentOpenFeignApplication.class, args);
+    }
+
+}
---
+++ BeginningSpringBoot/payment_open_feign/src/main/java/com/example/controllers/PaymentsController.java
@@ -0,0 +1,31 @@
+package com.example.controllers;
+
+import com.example.model.Payment;
+import com.example.proxy.PaymentsProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class PaymentsController {
+
+    private final PaymentsProxy paymentsProxy;
+
+    @Autowired
+    public PaymentsController(
+        PaymentsProxy paymentsProxy
+    ) {
+        this.paymentsProxy = paymentsProxy;
+    }
+
+    @PostMapping("/payment")
+    public Payment createPayment(
+        @RequestBody Payment payment
+    ) {
+        String requestId = UUID.randomUUID().toString();
+        return this.paymentsProxy.createPayment(requestId, payment);
+    }
+}
