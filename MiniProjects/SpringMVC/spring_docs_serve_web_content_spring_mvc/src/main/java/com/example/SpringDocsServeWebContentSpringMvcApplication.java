a5711234664e04cb4a9a61dd29546f1de5044a2f
Eric Ricketts <eric_ricketts@me.com>
2022-05-25 17:10:00 -0400

    spring documentation serving pages with spring mvc

---
+++ MiniProjects/SpringMVC/spring_docs_serve_web_content_spring_mvc/src/main/java/com/example/SpringDocsServeWebContentSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocsServeWebContentSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDocsServeWebContentSpringMvcApplication.class, args);
+	}
+
+}
